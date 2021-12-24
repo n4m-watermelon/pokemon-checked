84ef499fcc57f4e0206bdf1fc4a5677a2acb39b0
le doan nam <namthamlam505@gmail.com>
2021-12-24 09:36:11 +0700

    setup build

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: 'https://n4m-watermelon.github.io/pokemon-checked/'
 })
